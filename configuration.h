792d104e942ff546eef80ca7e8a0e3fecc85405e
joe fisher <joefisher@Joes-MBP.home>
2018-01-19 21:29:52 -0800

    hi

--- configuration.h
+++ configuration.h
@@ -680,7 +680,7 @@ Value is used for all generic tables created. */
 // ############# Heated bed configuration ########################
 
 /** \brief Set true if you have a heated bed connected to your board, false if not */
-#define HAVE_HEATED_BED false
+#define HAVE_HEATED_BED true
 
 #define HEATED_BED_MAX_TEMP 120
 /** Skip M190 wait, if heated bed is already within x degrees. Fixed numbers only, 0 = off. */
